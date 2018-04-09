0386dd2ea972704d850077e5b148441849302931
Otsuki Hitoshi <hitochan777@gmail.com>
2018-04-10 00:49:39 +0900

    Add reference for ambient module to resolve hubot

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,14 +1,16 @@
 // @format
 
-import * as TypeMoq from "typemoq"
-// import * as hubot from "hubot"
+/// <reference path="@types/hubot/index.d.ts" />
 
-import { RedisRepository } from "."
+import * as TypeMoq from "typemoq"
+import * as hubot from "hubot"
+
+import { RedisRepository } from "./index"
 
 describe("redis repository", () => {
   it("", () => {
     // const robot: TypeMock.Mock<hubot.Robot> = TypeMoq.Mock.ofType(hubot.Robot)
     // robot.brain.set("a", "b")
-    const a: string = 2
+    const a: string = "hoge"
   })
 })
